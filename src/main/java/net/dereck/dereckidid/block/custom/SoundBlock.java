f91e413d8635902c57e2e258eff3e13f17f496e8
Dereck Angeles <dereckangeles15071998@gmail.com>
2023-11-26 14:43:15 -0700

    Agregando comentarios a las erramientas anadidas

--- src/main/java/net/dereck/dereckidid/block/custom/SoundBlock.java
+++ src/main/java/net/dereck/dereckidid/block/custom/SoundBlock.java
@@ -1,11 +1,17 @@
 package net.dereck.dereckidid.block.custom;
 
+import java.util.List;
+
 import net.minecraft.core.BlockPos;
+import net.minecraft.network.chat.Component;
 import net.minecraft.sounds.SoundEvents;
 import net.minecraft.sounds.SoundSource;
 import net.minecraft.world.InteractionHand;
 import net.minecraft.world.InteractionResult;
 import net.minecraft.world.entity.player.Player;
+import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.item.TooltipFlag;
+import net.minecraft.world.level.BlockGetter;
 import net.minecraft.world.level.Level;
 import net.minecraft.world.level.block.Block;
 import net.minecraft.world.level.block.state.BlockState;
@@ -22,6 +28,11 @@ public class SoundBlock extends Block{
                 pLevel.playSound(pPlayer, pPos, SoundEvents.NOTE_BLOCK_DIDGERIDOO.get(), SoundSource.BLOCKS, 1.0f, 1.0f);
         return InteractionResult.SUCCESS;
 
+    }
+    @Override
+    public void appendHoverText(ItemStack pStack, BlockGetter pLevel, List<Component> pTooltip, TooltipFlag pFlag) {
+        pTooltip.add(Component.literal("Makes sweet sound when right clicked"));
+        super.appendHoverText(pStack, pLevel, pTooltip, pFlag);
     }
     
 }
--- src/main/java/net/dereck/dereckidid/item/custom/MetalDetectorItem.java
+++ src/main/java/net/dereck/dereckidid/item/custom/MetalDetectorItem.java
@@ -1,12 +1,17 @@
 package net.dereck.dereckidid.item.custom;
 
+import java.util.List;
+
 import net.minecraft.client.resources.language.I18n;
 import net.minecraft.core.BlockPos;
 import net.minecraft.network.chat.Component;
 import net.minecraft.world.InteractionResult;
 import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.item.Item;
+import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.item.TooltipFlag;
 import net.minecraft.world.item.context.UseOnContext;
+import net.minecraft.world.level.Level;
 import net.minecraft.world.level.block.Block;
 import net.minecraft.world.level.block.Blocks;
 import net.minecraft.world.level.block.state.BlockState;
@@ -40,6 +45,13 @@ public class MetalDetectorItem extends Item {
         
         pContext.getItemInHand().hurtAndBreak(1, pContext.getPlayer(), player -> player.broadcastBreakEvent(player.getUsedItemHand()));
         return InteractionResult.SUCCESS;
+    }
+
+    @Override
+    public void appendHoverText(ItemStack pStack, Level pLevel, List<Component> pTooltipComponents, TooltipFlag pIsAdvanced) {
+
+        pTooltipComponents.add(Component.translatable("tooltip.dereckidid.metal_detector.tooltip"));
+        super.appendHoverText(pStack, pLevel, pTooltipComponents, pIsAdvanced);
     }
 
     private void outputValuableCoordinates(Player player, BlockPos blockpos, Block block) {
